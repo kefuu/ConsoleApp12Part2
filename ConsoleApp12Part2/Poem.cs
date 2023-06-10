ab0d7f5be4d1b542684b2bb3d2aa533f7985a911
creepye <creepye@DESKTOP-TR3IAKP>
2023-06-11 00:21:10 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp12Part2/Poem.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+
+namespace ConsoleApp12Part2
+{
+    public class Poem
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int YearOfWriting { get; set; }
+        public string Text { get; set; }
+        public string Theme { get; set; }
+    }
+
+    public class PoemManager
+    {
+        private List<Poem> poems;
+
+        public PoemManager()
+        {
+            poems = new List<Poem>();
+        }
+
+        public void AddPoem(Poem poem)
+        {
+            poems.Add(poem);
+        }
+
+        public void DeletePoem(Poem poem)
+        {
+            poems.Remove(poem);
+        }
+
+        public void UpdatePoem(Poem oldPoem, Poem newPoem)
+        {
+            int index = poems.IndexOf(oldPoem);
+            if (index != -1)
+            {
+                poems[index] = newPoem;
+            }
+        }
+
+        public IEnumerable<Poem> Search(Func<Poem, bool> predicate)
+        {
+            return poems.FindAll(new Predicate<Poem>(predicate));
+        }
+
+        public void SaveToFile(string filename)
+        {
+            var json = JsonConvert.SerializeObject(poems);
+            File.WriteAllText(filename, json);
+        }
+
+        public void LoadFromFile(string filename)
+        {
+            if (File.Exists(filename))
+            {
+                var json = File.ReadAllText(filename);
+                poems = JsonConvert.DeserializeObject<List<Poem>>(json);
+            }
+        }
+
+        public List<Poem> ReportByTitle()
+        {
+            return poems.OrderBy(p => p.Title).ToList();
+        }
+
+        public List<Poem> ReportByAuthor()
+        {
+            return poems.OrderBy(p => p.Author).ToList();
+        }
+
+        public List<Poem> ReportByTheme()
+        {
+            return poems.OrderBy(p => p.Theme).ToList();
+        }
+
+        public void DisplayReport(List<Poem> report)
+        {
+            foreach (var poem in report)
+            {
+                Console.WriteLine($"Title: {poem.Title}, Author: {poem.Author}, Year: {poem.YearOfWriting}, Theme: {poem.Theme}");
+                Console.WriteLine($"Text: {poem.Text}\n");
+            }
+        }
+
+        public void SaveReportToFile(string filename, List<Poem> report)
+        {
+            var json = JsonConvert.SerializeObject(report);
+            File.WriteAllText(filename, json);
+        }
+    }
+}
---
+++ ConsoleApp12Part2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ConsoleApp12Part2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var manager = new PoemManager();
+
+            var poem1 = new Poem { Title = "Sonnet 18", Author = "William Shakespeare", YearOfWriting = 1609, Text = "Shall I compare thee to a summer's day...", Theme = "Love" };
+            var poem2 = new Poem { Title = "The Road Not Taken", Author = "Robert Frost", YearOfWriting = 1916, Text = "Two roads diverged in a yellow wood...", Theme = "Life Choices" };
+            var poem3 = new Poem { Title = "The Raven", Author = "Edgar Allan Poe", YearOfWriting = 1845, Text = "Once upon a midnight dreary...", Theme = "Loss and Mourning" };
+
+            manager.AddPoem(poem1);
+            manager.AddPoem(poem2);
+            manager.AddPoem(poem3);
+
+            manager.DeletePoem(poem2);
+
+            var updatedPoem = new Poem { Title = "Sonnet 18", Author = "William Shakespeare", YearOfWriting = 1609, Text = "Shall I compare thee to a summer's day? Thou art more lovely...", Theme = "Love" };
+            manager.UpdatePoem(poem1, updatedPoem);
+
+            var results = manager.Search(p => p.Author == "William Shakespeare" && p.YearOfWriting == 1609 && p.Theme == "Love");
+            foreach (var poem in results)
+            {
+                Console.WriteLine($"Title: {poem.Title}, Author: {poem.Author}, Year: {poem.YearOfWriting}, Theme: {poem.Theme}");
+                Console.WriteLine($"Text: {poem.Text}\n");
+            }
+
+            var titleReport = manager.ReportByTitle();
+            Console.WriteLine("\n=== Report by Title ===");
+            manager.DisplayReport(titleReport);
+
+            var authorReport = manager.ReportByAuthor();
+            Console.WriteLine("\n=== Report by Author ===");
+            manager.DisplayReport(authorReport);
+
+            var themeReport = manager.ReportByTheme();
+            Console.WriteLine("\n=== Report by Theme ===");
+            manager.DisplayReport(themeReport);
+
+            manager.SaveReportToFile("theme_report.json", themeReport);
+
+            manager.SaveToFile("poems.json");
+            manager.LoadFromFile("poems.json");
+
+            Console.WriteLine("\nFinished execution. Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
